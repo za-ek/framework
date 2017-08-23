336191581f8663a501f937757c44ab9e04e0cf7a
za-ek <tz@za-ek.ru>
2017-08-23 22:33:07 +0300

    Тестирование конфигурации

--- zaek/bin/controller.php
+++ zaek/bin/controller.php
@@ -3,6 +3,7 @@ require __DIR__ . '/../lib/zaek/engine/cmain.php';
 $app = new \zaek\engine\CMain();
 
 $app->conf()->addFile(__DIR__ . '/../conf/default.ini.php', 'ini');
+$app->conf()->addFile($_SERVER['DOCUMENT_ROOT'] . '/config.ini.php', 'ini');
 $app->conf()->push([
     'request' => [
         'uri' => $_SERVER["REQUEST_URI"] ?? $_SERVER["SCRIPT_NAME"]
