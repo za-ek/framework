5fc249c96c6e1bde68f1d428650af84721dab402
za-ek <tz@za-ek.ru>
2017-09-04 10:08:00 +0300

    Подключение шаблона

--- templates/default/template.php
+++ templates/default/template.php
@@ -1,1 +1,15 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Шаблон za-ek</title>
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
 <?php
+$this->includeFile($this->pathFromUri($this->conf()->get('request', 'uri')));
+?>
+</body>
+</html>
