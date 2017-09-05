ff5dd13b8cef66a9640c66391c3078d805f5716d
za-ek <tz@za-ek.ru>
2017-09-05 23:02:01 +0300

    Добавлен хэш пароля

--- zaek/tmp/data/users.ini.php
+++ zaek/tmp/data/users.ini.php
@@ -1,4 +1,4 @@
 ; <?php die(); ?>
-fields='id;login;password'
-line[]='1;root;qwerty1'
-line[]='2;admin;qwerty'
+fields='id;login;password;salt;hash'
+line[]='1;root;qwerty1;2h3$xmW;e4f9675a4f56b031571bd7d1d1ecb4542eee58f8177f726bd4065499dcdb06c7'
+line[]='2;admin;qwerty;*(l&36j;407da7341b204fbabf861309d10be93ac25d1bc63eb7852e5c4e55aa7948fd0f'
