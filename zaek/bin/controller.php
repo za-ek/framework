4acbef2d5122d4096df67f22364e8f3bb00a4726
za-ek <tz@za-ek.ru>
2017-09-08 10:30:23 +0300

    Статические файлы и объект шаблона

--- zaek/bin/controller.php
+++ zaek/bin/controller.php
@@ -1,5 +1,7 @@
 <?php
 require __DIR__ . '/../lib/zaek/engine/cmain.php';
+
+// Пример переопределения коннектора по умолчанию
 $app = new class extends \zaek\engine\CMain{
     public function data()
     {
@@ -7,6 +9,55 @@ $app = new class extends \zaek\engine\CMain{
             $this->_data = new \zaek\data\mysqli\CConnector($this);
         }
         return parent::data();
+    }
+    public function template()
+    {
+        if ( is_null($this->_template) ) {
+            $this->_template = new \zaek\engine\CTemplate($this);
+        }
+
+        return parent::template();
+    }
+
+    /**
+     * Если раздача статики не происходит без обработки сервером, например, с помощью nginx
+     *
+     * @param $uri
+     * @return string
+     */
+    public function route($uri)
+    {
+        if ( strpos($uri, '?') ) {
+            $path = substr($uri, 0, strpos($uri, '?'));
+            $extension = $this->fs()->extension($path);
+        } else {
+            $path = $uri;
+            $extension = $this->fs()->extension($uri);
+        }
+
+
+        if ( $extension ) {
+            $aStaticExtensions = [
+                'js' => 'application/javascript',
+                'css' => 'text/css',
+                'woff' => 'application/x-font-woff',
+                'woff2' => 'application/x-font-woff2',
+                'ttf' => 'application/x-font-ttf',
+                'png' => 'image/png',
+                'gif' => 'image/gif',
+                'jpg' => 'image/jpg',
+                'jpeg' => 'image/jpg',
+            ];
+
+            if (array_key_exists($extension, $aStaticExtensions)) {
+                $this->template()->end();
+                header('Content-type: ' . $aStaticExtensions[$extension]);
+                include $path;
+                die();
+            }
+        }
+
+        return parent::route($uri);
     }
 };
 
