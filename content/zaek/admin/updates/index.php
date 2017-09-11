9fae6e0a01b9ec40a9573eec890feb48584cc8ff
za-ek <tz@za-ek.ru>
2017-09-11 12:38:03 +0300

    Расширения zaek

--- content/zaek/admin/updates/index.php
+++ content/zaek/admin/updates/index.php
@@ -7,8 +7,8 @@
             <div class="box-body">
                 <table class="table table-hover table-row-click">
                     <tbody>
-                    <tr data-type="github">
-                        <td>Из репозитория github</td>
+                    <tr data-source="zaek">
+                        <td>Расширения zaek</td>
                     </tr>
                     </tbody>
                 </table>
@@ -17,21 +17,27 @@
         </div>
     </div>
     <div class="col-lg-6" id="update_detail">
-        Выберите способ установки
+        <div class="box fixed-box">
+            <div class="box-header">
+                <h3 class="box-title">Выберите способ установки</h3>
+            </div>
+            <div class="box-body">
+
+            </div>
+            <div class="overlay"><i class="fa fa-spin fa-refresh"></i></div>
+        </div>
     </div>
 </section>
 
 <script>
     $(function() {
         $('.table-row-click tr').click(function() {
-            var t = $(this).attr('data-type');
-            $.ajax({
-                url: '/zaek/admin/updates/types/' + t + '.php',
-                data : {
-                    'zAjax' : 1
-                }
-            }).done(function(e) {
-                $('#update_detail').html(e);
+            var t = $(this).attr('data-source');
+            $('#update_detail > .box > .box-body').zAjax({
+                url: '/zaek/admin/updates/types/' + t + '.php'
+            }, function(r) {
+                $('#update_detail .box-body').html(r.result);
+                $('#update_detail .box-title').html(r.page_param.title);
             });
         });
     });
---
+++ content/zaek/admin/updates/types/zaek.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @var $this \zaek\engine\CMain
+ */
+$this->template()->setTitle('Модули zaek');
+?>
+<table class="table">
+    <tbody>
+    <tr>
+        <td>Модуль для разработки сервисов</td>
+        <td>
+            <?php
+            if ( file_exists($this->fs()->convertPath('%ADMIN_ROOT%/updates/current/zaek_dev.php')) ) {
+                echo '<a href="#" class="update_module" data-module="za-ek/zaek_dev" data-source="github">Обновить</a> <small>Текущая версия: '.(include $this->fs()->convertPath('%ADMIN_ROOT%/updates/current/zaek_dev.php')).'</small>';
+            } else {
+                echo '<a href="#" class="update_module" data-module="za-ek/zaek_dev" data-source="github">Установить</a>';
+            }
+            ?>
+        </td>
+    </tr>
+    </tbody>
+</table>
+<script>
+$('.update_module').click(function() {
+    $(this).zAjax({
+        url : '/zaek/admin/updates/run.php',
+        params : {
+            module : $(this).attr('data-module'),
+            source : $(this).attr('data-source')
+        }
+    }, function(e) {
+        if ( !e.error ) {
+            alert('Обновлено');
+        } else {
+            alert('Ошибка');
+        }
+        console.log(e);
+    })
+});
+</script>
