ffc6483cc5d3769546f79f4df9c08ace233b9355
za-ek <tz@za-ek.ru>
2017-09-13 14:36:07 +0300

    Путь к статическим файлам нужно выдавать через путь к сайту, не относительным (для SEO) Метод clearProp для тестов

--- zaek/lib/zaek/engine/ctemplate.php
+++ zaek/lib/zaek/engine/ctemplate.php
@@ -48,9 +48,11 @@ class CTemplate extends CBuffer
                 if (strlen($value) > 0) {
                     if (substr($value, 0, 1) == '\\') {
                         $value =
-                            $this->_app->conf()->get('template', 'template_root') . '/' .
-                            $this->_app->conf()->get('template', 'code') .
-                            '/' . $type . '/' . substr($value, 1);
+                            $this->_app->fs()->getRelativePath(
+                                $this->_app->conf()->get('template', 'template_root') . '/' .
+                                $this->_app->conf()->get('template', 'code') .
+                                '/' . $type . '/' . substr($value, 1)
+                            );
                     } else if (
                         (substr($value, 0, 2) != '//') &&
                         (substr($value, 0, 4) != 'http')
@@ -60,7 +62,7 @@ class CTemplate extends CBuffer
                 }
 
                 if ( (substr($value, 0, 4) != 'http') ) {
-                    $value = 'http'.(($_SERVER['SERVER_PORT'] == 443 || isset($_SERVER['HTTPS']) || isset($_SERVER['HTTP_S'])) ? 's' : '' ).'://' . $_SERVER['SERVER_NAME'] . $value;
+                    $value = $this->getWebPath($value);
                 }
 
                 if ($type == 'css') {
@@ -150,8 +152,12 @@ class CTemplate extends CBuffer
     }
     public function img($img)
     {
-        return $this->_app->conf()->get('template', 'template_root') . '/' .
-            $this->_app->conf()->get('template', 'code') . '/images/'. $img;
+        return $this->getWebPath(
+            $this->_app->fs()->getRelativePath(
+                $this->_app->conf()->get('template', 'template_root') . '/' .
+                $this->_app->conf()->get('template', 'code') . '/images/'. $img
+            )
+        );
     }
     public function getCss()
     {
@@ -164,5 +170,17 @@ class CTemplate extends CBuffer
     public function getMeta()
     {
         return $this->_meta;
+    }
+    public function getWebPath($value)
+    {
+        return 'http'.(($_SERVER['SERVER_PORT'] == 443 || isset($_SERVER['HTTPS']) || isset($_SERVER['HTTP_S'])) ? 's' : '' ).'://' . $value;
+    }
+    public function clearProp($type)
+    {
+        if ( in_array([
+            'css','js','meta'
+        ], $type)) {
+            $this->{$type} = [];
+        }
     }
 }
