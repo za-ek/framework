d8aba16ec5a02ed5e5658347458ff2a23d2e31bf
Zakhar <admin@za-ek.ru>
2017-10-01 11:34:07 +0300

    fix
    mv: cannot move ‘zaek_start-master/zaek’ to ‘./zaek’: File exists

--- before_install.sh
+++ before_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 wget https://github.com/za-ek/zaek_start/archive/master.zip -O zaek_start.zip
 unzip zaek_start
-mv zaek_start-master/* ./
+cp -r zaek_start-master/* ./
+rm -rf zaek_start-master/*
