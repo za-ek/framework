b5a2e6255ac2bbbba826520bd2a07136392051a6
Zakhar Telyatnikov <tz@za-ek.ru>
2018-09-07 19:07:02 +0300

    Error on empty list

--- Zaek/Kernel/Request.php
+++ Zaek/Kernel/Request.php
@@ -5,51 +5,17 @@ use Zaek\Engine\Main;
 
 class Request
 {
+    /**
+     * @var Main
+     */
     protected $_app;
 
+    /**
+     * Request constructor.
+     * @param Main $app
+     */
     public function __construct(Main $app)
     {
         $this->_app = $app;
-    }
-
-    public function cookie()
-    {
-        if(func_num_args() == 1) {
-            return $_COOKIE[func_get_arg(0)] ?? null;
-        }
-
-        return $_COOKIE;
-    }
-
-    public function post()
-    {
-        if(func_num_args() == 1) {
-            return $_POST[func_get_arg(0)] ?? null;
-        }
-
-        return $_POST;
-    }
-
-    public function get()
-    {
-        if(func_num_args() == 1) {
-            return $_GET[func_get_arg(0)] ?? null;
-        }
-
-        return $_GET;
-    }
-
-    public function bin()
-    {
-        return $_FILES;
-    }
-
-    public function server()
-    {
-        if(func_num_args() == 1) {
-            return $_SERVER[func_get_arg(0)] ?? null;
-        }
-
-        return $_SERVER;
     }
 }
